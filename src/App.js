9233bd4e0a37a041c565e83169f11b36e37d920f
Shreyas M <shreyas.m246418@gmail.com>
2024-12-04 12:36:37 +0530

    app.js update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
@@ -7,7 +7,7 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <Router basename="/spa-gh">
+    <Router>
       <div>
         <Navbar />
         <Routes>
